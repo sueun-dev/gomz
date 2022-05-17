3100f2c4ad2c43f5d845d7e43d70fa8ef7fe10c0
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2022-05-17 21:55:38 +0900

    Update GomzV1Sale.sol

--- src/contracts/NFT/Sale/GomzV1Sale.sol
+++ src/contracts/NFT/Sale/GomzV1Sale.sol
@@ -10,18 +10,16 @@ import "../ERC721/IGomzV1.sol";
 contract GomzSale is Context {
     using SafeMath for uint256;
 
-    uint256 blocknumber;
-    uint256 blockDifficult;
-
     IGomzV1 public GomzNFTContract;
 
     uint16 MAX_SUPPLY = 2022;
-    uint256 PRICE_PER_ETH = 0.02 ether;
+    uint256 PRICE_PER_ETH = 0.8 ether;
 
     mapping(address => bool) public whitelisted;
     uint256 public numWhitelisted;
-    uint16 WL_MAX_SUPPLY = 7;
-    uint256 WL_PRICE_PER_ETH = 0.01 ether;
+    //N개 까지! (600개는 먼저 가져가고 600개는 후 화리 컨트렉트)
+    uint16 WL_MAX_SUPPLY = 1200;
+    uint256 WL_PRICE_PER_ETH = 0.5 ether;
 
     uint256 public constant maxPurchase = 3;
 
@@ -29,8 +27,6 @@ contract GomzSale is Context {
     bool public WLisSale = false;
 
     address public C1;
-    address public C2;
-    address public C3;
 
     modifier mintRole(uint256 numberOfTokens) {
         require(isSale, "The sale has not started.");
@@ -45,7 +41,7 @@ contract GomzSale is Context {
         );
         _;
     }
-        //MAX_SUPPLY 는 4개로 설정
+
     modifier WLmintRole(uint256 numberOfTokens) {
         require(whitelisted[msg.sender] == true, "You are not white list");
         require(WLisSale, "The sale has not started.");
@@ -77,10 +73,10 @@ contract GomzSale is Context {
         _;
     }
 
-    // C1: Developer, C2: Developer, C3: Artist
+    //C1 = Creator1
     modifier onlyCreator() {
         require(
-            C1 == _msgSender() || C2 == _msgSender() || C3 == _msgSender(),
+            C1 == _msgSender(),
             "onlyCreator: caller is not the creator"
         );
         _;
@@ -91,26 +87,12 @@ contract GomzSale is Context {
         _;
     }
 
-    modifier onlyC2() {
-        require(C2 == _msgSender(), "only C2: caller is not the C2");
-        _;
-    }
-
-    modifier onlyC3() {
-        require(C3 == _msgSender(), "only C3: caller is not the C3");
-        _;
-    }
-
     constructor(
         address nft,
-        address _C1,
-        address _C2,
-        address _C3
+        address _C1
     ) {
         GomzNFTContract = IGomzV1(nft);
         C1 = _C1;
-        C2 = _C2;
-        C3 = _C3;
     }
 
     function mintByETH(uint256 numberOfTokens)
@@ -153,23 +135,13 @@ contract GomzSale is Context {
 
     function withdraw() public payable onlyCreator {
         uint256 contractETHBalance = address(this).balance;
-        uint256 percentageETH = contractETHBalance / 100;
+        uint256 percentageETH = contractETHBalance;
 
-        require(payable(C1).send(percentageETH * 35));
-        require(payable(C2).send(percentageETH * 35));
-        require(payable(C3).send(percentageETH * 30));
+        require(payable(C1).send(percentageETH));
     }
 
     function setC1(address changeAddress) public onlyC1 {
         C1 = changeAddress;
-    }
-
-    function setC2(address changeAddress) public onlyC2 {
-        C2 = changeAddress;
-    }
-
-    function setC3(address changeAddress) public onlyC3 {
-        C3 = changeAddress;
     }
 
     function setSale() public onlyCreator {
@@ -181,11 +153,11 @@ contract GomzSale is Context {
     }
 
     function publicSale() public onlyCreator{
-        PRICE_PER_ETH = 0.02 ether;
+        PRICE_PER_ETH = 0.8 ether;
     }
 
     function WLpublicSale() public onlyCreator { 
-        WL_PRICE_PER_ETH = 0.01 ether;
+        WL_PRICE_PER_ETH = 0.5 ether;
     }
 
     function getWLpublicSale() public view returns (uint256) {
@@ -196,14 +168,8 @@ contract GomzSale is Context {
         return PRICE_PER_ETH;
     }
 
-    function increasePrice(uint256 increaPrice) public onlyCreator {
-        increaPrice = increaPrice * (1 ether);
-        PRICE_PER_ETH += increaPrice;
-    }
-
-    function decreasePrice(uint256 decreaPrice) public onlyCreator {
-        decreaPrice = decreaPrice * (1 ether);
-        PRICE_PER_ETH -= decreaPrice;
+    function increasePrice() public onlyCreator {
+        PRICE_PER_ETH += 0.2 ether;
     }
 
     function addWhitelist(address[] memory _users) public onlyCreator {
@@ -224,23 +190,4 @@ contract GomzSale is Context {
           whitelisted[user] = false;
         }
     }
-
-  /*  function BlockNumber() public onlyCreator { 
-        blocknumber = block.number;
-    }
-
-    function getBlockNumber() public view returns (uint256) {
-        return blocknumber;
-    }
-
-    function BlockDifficulty() public onlyCreator { 
-        blockDifficult = block.difficulty;
-    }
-
-    function getBlockDifficulty() public view returns (uint256) {
-        return blockDifficult;
-    } */
-
-    
-
 }
