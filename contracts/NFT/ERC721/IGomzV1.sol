69a9ce56169975d386b6231b9b9498b541dbdf4e
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2022-05-21 19:23:04 +0900

    Update IGomzV1.sol

--- contracts/NFT/ERC721/IGomzV1.sol
+++ contracts/NFT/ERC721/IGomzV1.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/sueun-dev/gomz/blob/main/src/contracts/openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/sueun-dev/gomz/blob/main/src/contracts/openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "https://github.com/sueun-dev/gomz/blob/main/contracts/openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/sueun-dev/gomz/blob/main/contracts/openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 interface IGomzV1 is IERC721, IERC721Enumerable {
     function mint(address to) external;
